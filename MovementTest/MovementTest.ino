d530a0421169fd777972c7a19febca5920ec9c7d
Spencer <sta7ey@virginia.edu>
2017-04-24 12:07:14 -0400

    Minor Bug Fixes

--- MovementTest/MovementTest.ino
+++ MovementTest/MovementTest.ino
@@ -2,6 +2,7 @@
 
 EventTimer calibrator;
 
+// pin numbers
 int PWMright = 5;
 int rightIn2 = 4;
 int rightIn1 = 2;
@@ -16,7 +17,8 @@ int rightPhoto = 12;
 unsigned long leftMax = 0;
 unsigned long rightMax = 0;
 
-int photoBuffer = 1000;
+
+//int photoBuffer = 1000;
 // current speed
 int currSpeed = 100;
 
@@ -30,7 +32,8 @@ void setup() {
   pinMode(PWMleft, INPUT);
   pinMode(leftIn2, INPUT);
   pinMode(leftIn1, INPUT);
-  
+
+  // turn wheels to start moving forward
   digitalWrite(rightIn2, LOW);
   digitalWrite(rightIn1, HIGH);
   digitalWrite(leftIn2, LOW);
@@ -42,6 +45,7 @@ void setup() {
   //calibrate line sensor
   leftMax = calibrate(leftPhoto);
   rightMax = calibrate(rightPhoto);
+  
   Serial.print("Left Max: ");
   Serial.println(leftMax);
   Serial.print("Right Max: ");
@@ -52,18 +56,17 @@ void setup() {
 
 // check line sensor
 unsigned long checkSensor(int pin){
-    // left photosensor
-  pinMode(pin, OUTPUT);     //set to output
-  digitalWrite(pin, HIGH);  //set high
+  pinMode(pin, OUTPUT);           //set to output
+  digitalWrite(pin, HIGH);        //set high
   delay(1);                       //charge cap
  
-  pinMode(pin, INPUT);      //set low
+  pinMode(pin, INPUT);            //set low
   unsigned long s = micros();     //start timing
   
   while(digitalRead(pin) == HIGH){ 
   }
-  return micros() - s; // find final time
-}
+  return micros() - s;            // find final time
+} 
 
 
 // calibrate line sensor
@@ -88,28 +91,48 @@ unsigned long calibrate(int pin){
 
 void loop() {
   // check if on line
-  int left = checkSensor(leftPhoto);
-  int right = checkSensor(rightPhoto);
+  unsigned long left = checkSensor(leftPhoto);
+  unsigned long right = checkSensor(rightPhoto);
+  
   Serial.print(left);
   Serial.print("   ");
   Serial.println(right);
-  if (left <  (2000) && right < (2000)) {
-    // continue if on line
+
+  // continue if on line
+   if (left < 1200) && right < 2000) {
     analogWrite(PWMright, currSpeed);
     analogWrite(PWMleft, currSpeed); 
+    
     //Serial.println("On line");
     //Serial.println(left);
     //Serial.println(right);
   }
-  if (left > (1200)){
-    //slow left wheel if not
+  
+  // enter intersection
+  else if (left > 1200) && right > 2000) {
+    // stop
+    digitalWrite(right1In, LOW);
+    digitalWrite(left1In, LOW);
+
+    // continue if not turn signal
+    if (
+    digitalWrite(right1In, HIGH);
+    digitalWrite(left1In, HIGH);
+    
+    // check if turn right or turn left
+
+  }
+  
+  //slow left wheel if left sensor is high
+  else if (left > 1200){
     analogWrite(PWMleft, currSpeed - 28);  // 28 = 1 V for 9 V source
     //Serial.println("Left wheel off"); 
     //Serial.println(left); 
   }
-  if (right > (2000)){
-    //slow right wheel if not
-    analogWrite(PWMright, currSpeed - 38);
+
+  //slow right wheel if right sensor is high
+  else if (right > 2000){
+    analogWrite(PWMright, currSpeed - 28);
     //Serial.println("Right wheel off"); 
     //Serial.println(right);
   }
